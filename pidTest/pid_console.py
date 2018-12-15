f59550be5935d3298d112cc3a1ea1826fd855439
schuub <34111742+schuub@users.noreply.github.com>
2018-12-15 14:30:49 +0100

    Update pid_console.py

--- pidTest/pid_console.py
+++ pidTest/pid_console.py
@@ -3,8 +3,13 @@
 import os
 import sys
 from time import sleep
-from ev3dev2.sensor import INPUT_1, INPUT_3
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.button import Button
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.motor import MoveSteering
+from ev3dev2.sound import Sound
+
 
 
 def debug_print(*args, **kwargs):
@@ -16,42 +21,85 @@ def debug_print(*args, **kwargs):
 
 
 def main():
-    color = ColorSensor(INPUT_1)
-    kP = 4
-    kI = 2
-    kD = 0
+    sensLight = ColorSensor(INPUT_1)
+#     sensColor = ColorSensor(INPUT_4)
+#     sensColor.mode = sensColor.MODE_COL_COLOR
+    btn = Button()
+    sound = Sound()
+    steerPair = MoveSteering(OUTPUT_B, OUTPUT_C)
+    
+#     lastColor = sensColor.color_name
 
-    target = 35
+    speed = 90
+    kP = 2
+    kI = 0.1
+    kD = 0.5
+
+    target = 30
     error = 0
     lastError = 0
     turn = 0
 
+    errorAccu = 0
     integral = 0
-    counter = 0
 
-    while True:
-        # Proportional
-        error = target - color.reflected_light_intensity
-        proportional = error * kP
+    sound.tone([(400,400,20), (800,800,20)])
 
-        # integral
-        integral = integral + error
-        integral = lastError * kI
+    while not btn.any():
+            sleep(0.1)
 
-        counter = counter + 1
+    sound.beep()
+    sleep(2)
 
-        # derivative
+    debug_print('Error; Proportial; Integral; Derivative; Turn')
+    while not btn.any():
+        lightValue = sensLight.reflected_light_intensity
+        # colorName = sensColor.color_name
+
+
+        # detect edge
+        if lightValue == 0:
+                steerPair.off()
+                sleep(0.01)
+                continue
+        
+        error = target - lightValue
+
+        errorAccu += error
         derivative = error - lastError
+
+        # limit integral
+        if errorAccu > 200:
+                errorAccu = 200
+        elif errorAccu < -200:
+                errorAccu = -200
+
+        # apply gains
+        proportional = error * kP
+        integral = errorAccu * kI
         derivative *= kD
 
+
+        # calculate turn value
         turn = proportional + integral + derivative
 
-        #debug_print('Error: {} P: {} I: {} D: {} Turn: {}'.format(error, proportional, integral, derivative, turn))
-        debug_print('Counter: {}'.format(counter))
+        # limit turn value
+        if turn > 100:
+                turn = 100
+        elif turn < -100:
+                turn = -100
 
-        lastError = error
+        steerPair.on(int(turn*1), speed)
+        debug_print('{}; {}; {}; {}; {}'.format(int(error), int(proportional), int(integral), int(derivative), int(turn)))
+        # if lastColor != colorName:
+        #         sound.speak(colorName)
 
-        sleep(0.5)
+        lastError = error       # set current error to lastError at the end of the loop
+        # lastColor = sensColor.color_name
+
+        sleep(0.02)
+
+
 
 
 if __name__ == '__main__':
