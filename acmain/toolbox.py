095f28a453136cfcd3645daf745baab8ad780f51
schuub <schubington@gmail.com>
2018-12-19 18:05:20 +0100

    add config class and object to toolbox
    from toolbox import cfg
    
    cfg.pidFast -> K values in list form

--- acmain/toolbox.py
+++ acmain/toolbox.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, json
 
 class Debug(object):
 
@@ -9,3 +9,16 @@ class Debug(object):
         This shows up in the output panel in VS Code.
         '''
         print(*args, **kwargs, file=sys.stderr)
+
+class Config(object):
+    def __init__(self):
+        # create config if not existing
+        with open('cfg/config.json') as f:
+            self.data = json.load(f)
+            self.pidFast = [self.data['pid']['fast']['kP'], self.data['pid']['fast']['kI'], self.data['pid']['fast']['kD']]
+            self.pidSlow = [self.data['pid']['slow']['kP'], self.data['pid']['slow']['kI'], self.data['pid']['slow']['kD']]
+    
+    def update(self):
+        self.__init__()
+
+cfg = Config()
