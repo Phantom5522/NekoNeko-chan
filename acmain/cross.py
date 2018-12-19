3047dbbbbb1b638f4a8cbb5161b8edcbdb3fe9de
Phantom5522 <39403487+Phantom5522@users.noreply.github.com>
2018-12-19 12:27:32 +0100

    Test implentation for nChan-class and changed to LF

---
+++ acmain/cross.py
@@ -0,0 +1,5 @@
+from toolbox import Debug
+
+class Cross(object):
+    def firstTurn():
+        Debug.print("Robo at Cross first Turn")
--- acmain/main.py
+++ acmain/main.py
@@ -4,27 +4,32 @@
 # import alles was wir brauchen
 from nekoneko_chan import NekoNekoChan
 from time import sleep
-
+from toolbox import Debug
 
 
 def main():
     nchan = NekoNekoChan() # create object instance of robot
 
-    nchan.sound.speak('ready')
+    # nchan.sound.speak('ready')
 
-    while not nchan.btn.any():
-        sleep(0.1)
+    # while not nchan.btn.any():
+    #     sleep(0.1)
 
-    nchan.sound.beep()
-    sleep(1)
+    # nchan.sound.beep()
+    # sleep(1)
 
 
-    while not nchan.btn.any():
-        nchan.followLine()
-        sleep(0.01)
+    # while not nchan.btn.any():
+    #     nchan.followLine()
+    #     sleep(0.01)
     
-    nchan.sound.speak('program end')
-        
+    # nchan.sound.speak('program end')
+
+    for x in range(20):
+        nchan.statemachine.transition("toCrossFirstTurn")
+        nchan.statemachine.execute()
+        Debug.print("Jetzt sollt etwas passiert sein")
+        sleep(0.5)
 
 # calibrate sensors
 
