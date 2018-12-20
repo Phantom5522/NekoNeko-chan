37e38651b56e4fa7946488b883181ed01dc30470
FreyaW0lf <Felizeug@gmx.de>
2018-12-20 22:15:45 +0100

    Struktur Cross komplett

--- acmain/cross.py
+++ acmain/cross.py
@@ -1,13 +1,15 @@
 from toolbox import Debug
-from toolbox import Debug
 from claw import Claw
 from statemachine import State, Transition, StateMachine
-from cross import Cross
 from drive import Drive
 
 class Cross(object):
 
     self.turncount = 0
+
+    drive = Drive()
+    drive.turn(10) # hat noch keinen Effekt
+
 
     # wenn Farbe 'blau':
         # fahre bis Mitte
@@ -25,7 +27,17 @@ class Cross(object):
                     # falls Hindernis:
                     # falls turncount == 1:
                     # search()
-            
+    # fahre zurück
+    # drehe um 180 Grad
+    # fahre geradeaus
+    # falls Farbe == 'blau':
+        # fahre bis Mitte
+        # falls turncount = 2:
+            # wenn Richtung == 'rechts'
+            # drehe rechts um 90 Grad
+            # elif Richtung == 'links'
+            # drehe links um 90 Grad
+        # folge Linie
         
 
     def firstTurn(self, sensorValues):
@@ -48,4 +60,12 @@ class Cross(object):
                         # minAbstand = Abstand
             # verringere Geschwindigkeit
             # fahre für 3 Sekunden
+        # grab()
         # setze turncount = 2
+
+    def grab():
+        # öffne Greifarm
+        # solange TouchButton = kein Signal:
+            # fahre langsam auf Ball zu
+        # wenn TouchButton == Signal:
+            # Greifarm schließen
