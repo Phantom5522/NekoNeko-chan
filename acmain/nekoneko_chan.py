91a29943a0121e1893054f3925c24b5becfb5f30
schuub <schubington@gmail.com>
2018-12-20 16:53:51 +0100

    added checkBlue() and cross transition

--- acmain/nekoneko_chan.py
+++ acmain/nekoneko_chan.py
@@ -45,6 +45,10 @@ class NekoNekoChan(object):
         self.fsm.transitions["toFollowLine"] = Transition("followLine")
         self.fsm.transitions["toCrossFirstTurn"] = Transition("crossFirstTurn")
         self.fsm.transitions["toBrake"] = Transition("brake", self.drive.brake)
+
+    def checkBlue(self):
+        hue = self.sensValues[]
+        return hue > 0.4 and hue < 0.68     # TODO: measure best threshold for blue values
         
     def run(self):
         while True:
@@ -73,6 +77,8 @@ class NekoNekoChan(object):
                 # line following
 
                 # intersection first turn
+            elif self.checkBlue():
+                self.fsm.transition("crossFirstTurn")
 
                 # detect ball
 
