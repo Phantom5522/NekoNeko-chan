80f4e15b14273efba77ac473ea3de946cf160e0e
schuub <schubington@gmail.com>
2018-12-20 13:14:02 +0100

    change Config to stativ variable
    use Config.update() once before accessing data

--- acmain/testConfig.py
+++ acmain/testConfig.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from toolbox import cfg, Debug
+from toolbox import Config, Debug
 
 Debug.print('Hello, i am connected')
 
-Debug.print(cfg.data["version"])
+Config.update()
+Debug.print(Config.data["version"])
