a3259aab1ac61b956a0fe28c82679a1d6981a8a2
Phantom5522 <phantom.5522@gmx.de>
2019-01-06 21:52:26 +0100

    Cross added TTE and distance

--- acmain/cross.py
+++ acmain/cross.py
@@ -1,5 +1,9 @@
 from toolbox import Debug
 
 class Cross(object):
+    def __init__(self):
+        self.distance = 0
+        self.turnsToExit = 0
+
     def firstTurn(self, sensorValues):
         Debug.print("Robo at Cross first Turn")
