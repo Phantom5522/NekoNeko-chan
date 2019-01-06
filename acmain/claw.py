2b43283953b1a0015f2fdb71bef7f5ccde0b4b9c
Phantom5522 <phantom.5522@gmx.de>
2019-01-06 21:52:52 +0100

    Claw added hasBall

--- acmain/claw.py
+++ acmain/claw.py
@@ -5,6 +5,7 @@ from ev3dev2.motor import MediumMotor, OUTPUT_A
 class Claw(object):
     def __init__(self):
         self.closed = False
+        self.hasBall = False
         self.motor = MediumMotor(OUTPUT_A)
     
     def releaseClaw(self):
@@ -13,9 +14,11 @@ class Claw(object):
         self.motor.off()
         self.closed = False
 
-    def closeClaw(self):
+    def closeClaw(self, isBallGrab = False):
         self.motor.on(50)
         self.motor.wait_until_not_moving(timeout=500)
         self.motor.off()
         self.closed = True
+        if isBallGrab:
+            self.hasBall = True
 
