9c46ccaf3e4c01f38e849eda746e836b5f2d533a
schuub <schubington@gmail.com>
2019-01-07 14:38:02 +0100

    Create tones.py

---
+++ acmain/tones.py
@@ -0,0 +1,5 @@
+# Format: [(frequency, duration, pause), (frequency, duration, pause)]
+# frequency in Hz, duration / pause in milliseconds
+
+class Tones(object):
+    test = [(800, 100, 100),(800, 100, 600),(800, 300, 100)]
